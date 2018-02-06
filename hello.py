a8e5436c9d98e6e385fbc2e463616da46600539c
qsef3546 <35215294+qsef3546@users.noreply.github.com>
2018-02-06 17:53:36 +0900

    hello.py modified on GitHub
    ...

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-// For command line git tutorial
-// Git Hub modification
+// For command line git tutorial.
+// Git Hub modification.
 print("Hello world")
 print("Good afternoon!!")
 print("Check point 1")
